9c27d706f40302a0c478e7e4da005847bc9800e1
Gaurav Chauhan <140693142+gaurav7chauhan@users.noreply.github.com>
2024-09-30 12:14:17 +0000

    heap(non-primitive) and stack(primitive)

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,4 +1,19 @@
-console.log(undefined > 0)
-console.log(undefined == 0)
-console.log(undefined === 0)
-console.log(undefined >= 0)
+// let firstName = "gaurav"
+
+// changeName = firstName
+// console.log("firstChange", changeName)
+// changeName = "chauhan"
+
+// console.log("firstName", firstName)
+// console.log("changeName", changeName)
+
+let userOne = {
+    email: "user@gmail.com",
+    address: "L-2/85 new delhi"
+}
+
+let userTwo = userOne
+
+userTwo.email = "userTwo@gmail.con"
+
+console.log(userOne)
