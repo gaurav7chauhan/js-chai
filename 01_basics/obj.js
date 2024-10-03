c182b4b2667dabeb7556d5aea0a8a50964c41ca2
Gaurav Chauhan <140693142+gaurav7chauhan@users.noreply.github.com>
2024-10-03 15:41:01 +0000

    touch json

--- 01_basics/obj.js
+++ 01_basics/obj.js
@@ -3,23 +3,15 @@
 // object literals  ====>  const obj = {}
 
 
-const arr = [
-    {
-        id: 1,
-        email: "gc@g.com"
-    },
-    {
-        id: 2,
-        email: "js@g.com"
-    },
-    {
-        id: 3,
-        email: "kjjk@g.com"
-    }
-]
 
-console.log(Object.keys(arr[1]))
-console.log(Object.values(arr[1]))
-console.log(Object.entries(arr[1]))
+const course = {
+    courseName: "javascript",
+    price: 999,
+    instructor: "hitesh sir"
+}
 
-console.log(arr[1].hasOwnProperty("id"))
+console.log(course.courseName)
+
+const {courseName: topic, price, instructor} = course
+
+console.log(topic)
